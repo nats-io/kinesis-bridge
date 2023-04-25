c0445cf9a1431648d6e80125825bc10d3c6a9eb0
Byron Ruth <byron@nats.io>
2023-04-25 09:53:18 -0400

    Update demo to 120 shards
    Signed-off-by: Byron Ruth <byron@nats.io>

--- demo/tf/kinesis.tf
+++ demo/tf/kinesis.tf
@@ -1,6 +1,6 @@
 resource "aws_kinesis_stream" "test_stream" {
   name             = "test-stream"
-  shard_count      = 10
+  shard_count      = 120
   retention_period = 24
 
   stream_mode_details {
